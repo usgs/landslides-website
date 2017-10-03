3f081c70ac03b1241429337101d748e43b6acf62
lisa-usgs <lisa@usgs.gov>
2017-10-03 16:27:39 -0600

    added prelim assessment page under research

--- src/htdocs/research/_navigation.inc.php
+++ src/htdocs/research/_navigation.inc.php
@@ -1,6 +1,8 @@
 <?php
 echo navGroup(navItem('/research/','Research'),
   navItem('/research/projects.php', 'Research Projects').
-  navItem('/research/software.php', 'Software')
+  navItem('/research/software.php', 'Software').
+  navItem('/research/maps.php', 'Maps').
+  navItem('/research/prelim.php', 'Preliminary Assessments')
 );
 ?>
