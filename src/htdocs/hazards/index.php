a93b23e6435291da40bb8a792a8261cfafd6acdf
lisa-usgs <lisa@usgs.gov>
2017-10-05 13:02:34 -0600

    moved orphaned page content to current active page

--- src/htdocs/hazards/index.php
+++ src/htdocs/hazards/index.php
@@ -31,3 +31,37 @@
     </ul>
   </div>
 </div>
+
+<h3><a href="whattodo.php">If you live in a recently burned area, and there is a rainstorm…</a></h3>
+
+<hr />
+
+<figure class='right'>
+  <img src="images/Mullally.jpg" alt=""/>
+  <figcaption>
+    Downstream impacts of a post-fire debris-flow in Mullally Canyon on February 6, 2010, near La Canada-Flintridge, California.  Debris flow was generated during a burst of high intensity rainfall over the area burned by the September 2009 Station fire.
+  </figcaption>
+</figure>
+
+
+<p>
+  Wildland fires are inevitable in the western United States. Expansion of human development into forested areas has created a situation where wildfires can adversely affect lives and property, as can the flooding and landslides that occur in the aftermath of the fires. There is a need to develop tools and methods to identify and quantify the potential hazards posed by landslides produced from burned watersheds. Post-fire landslide hazards include fast-moving, highly destructive debris flows that can occur in the years immediately after wildfires in response to high intensity rainfall events, and those flows that are generated over longer time periods accompanied by root decay and loss of soil strength. Post-fire debris flows are particularly hazardous because they can occur with little warning, can exert great impulsive loads on objects in their paths, can strip vegetation, block drainage ways, damage structures, and endanger human life. Wildfires could potentially result in the destabilization of pre-existing deep-seated landslides over long time periods.
+</p>
+<p>
+  The focus of this project is to develop tools and methods for the prediction of postwildfire landslide activity and hazard delineation. Personnel dealing with post-fire rehabilitation and emergency planning need tools to determine the both the probability and magnitude of such potentially destructive events, so we have developed methods to predict which basins might produce post-fire debris flows, and how big these events might be. By utilizing these methods, federal, state, and local land-management agencies can tailor debris-flow specific mitigation efforts to watersheds that are the most prone to the largest debris-flow events.
+</p>
+
+<iframe width="940" height="529" src="https://www.youtube.com/embed/OTuHQOHjC6Q" frameborder="0" allowfullscreen></iframe>
+<p>
+  The June 2016 Fish Fire burned over 12 km^2 in Los Angeles County, California. After the fire, the USGS installed an automated rain-triggered camera to monitor post-wildfire flooding and debris flow in a small canyon above the Las Lomas debris basin in Duarte. This video shows the peak flow triggered by an intense rainstorm on January 20, 2017.
+</p>
+
+<h2>Additional Information</h2>
+<ul>
+<li><a href="/monitoring/">Monitoring Post-Fire Flash Floods and Debris Flows</a></li>
+<li><a href="https://pubs.usgs.gov/fs/2005/3106/">Wildfires and Debris Flows in Southern California</a></li>
+</ul>
+
+<h2>Contact Information</h2>
+<p>
+<a href="mailto:jwkean@usgs.gov">Jason Kean</a> and <a href="mailto:dstaley@usgs.gov">Dennis Staley</a>, Landslide Hazards Program</p>
