5054b8bf7d44cb1c466506eff613966c4bc9ba92
lisa-usgs <lisa@usgs.gov>
2018-04-09 12:07:40 -0600

    added Science for Everyone link in Research leftnav

--- src/htdocs/research/_navigation.inc.php
+++ src/htdocs/research/_navigation.inc.php
@@ -3,6 +3,8 @@ echo navGroup(navItem('/research/','Research'),
   navItem('/research/projects.php', 'Research Projects').
   navItem('/research/software.php', 'Software').
   navItem('/research/maps.php', 'Maps').
-  navItem('/research/prelim.php', 'Preliminary Assessments')
+  navItem('/research/prelim.php', 'Preliminary Assessments').
+  navItem('/research/featured/', 'Science for Everyone')
+
 );
 ?>
