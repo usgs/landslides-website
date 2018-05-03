62a0545097e20f6a6df206ce5ff4511eb0b72503
lisa-usgs <lisa@usgs.gov>
2018-05-03 15:45:20 -0600

    added graph

--- src/htdocs/research/featured/clayey-ls/index.php
+++ src/htdocs/research/featured/clayey-ls/index.php
@@ -19,19 +19,30 @@
   Not all landslides occur as catastrophic events. Some creep along at a snail’s pace, stopping and starting, and not advancing more than 1 meter per year, and many of these are called earthflows.  Earthflows are extremely common all over the world, in fact they’re much more common than the fast-moving, deadly landslides we hear about often.  Since earthflows rarely cause loss of life, they aren’t in the news like their damaging counterparts. Despite the fact that earthflows are so common, scientists still haven’t completely nailed down the reasons why they are so sluggish and sometimes move in fits and starts. Two USGS geologists, Bill Schulz and Joel Smith, and several colleagues from other institutions, studied one of thousands of earthflows in Northern California by the name of Two Towers to see if they could learn more about what controls their behavior, and that of other slow-moving landslides.
 </p>
 <p>
-  The Two Towers earthflow is about 250m long, about 40m wide, and 7m deep and moves under 1m per year. It’s typical of many of the other earthflows in the area near Zenia, CA, and it was chosen for this study because it moves slowly, has a simple shape, and is small enough to accommodate the small number of instruments the scientists had available to dedicate to a multi-year study. Such a slow-moving landslide requires a long time and a lot of patience to collect adequate data for a study, and the team collected data for roughly 2-1/2 years from 2014 to 2017.  During this time, they collected hourly measurements of rainfall, landslide movement, landslide thickness changes, groundwater pressure, and swell pressure (the pressure exerted when the soil in the landslide expands, usually due to the presence of water).  They also collected samples of the material (dirt) from the landslide to analyze in a laboratory.  From these samples they learned about the water content, the density, shear strength (how well the material holds together under pressure), type of landslide materials and minerals (most importantly the percentage of clay minerals), the shrink-swell amount, and the swell pressure.
-</p>
+  The Two Towers earthflow is about 250m long, about 40m wide, and 7m deep and moves under 1m per year. It’s typical of many of the other earthflows in the area near Zenia, CA, and it was chosen for this study because it moves slowly, has a simple shape, and is small enough to accommodate the small number of instruments the scientists had available to dedicate to a multi-year study. Such a slow-moving landslide requires a long time and a lot of patience to collect adequate data for a study, and the team collected data for roughly 2-1/2 years from 2014 to 2017.
+<br/>
 
-<figure class='left'>
+<figure class='left' style="width: 300px;">
 <img src="images/view.jpg" alt=""/>
 <figcaption>
   Caption here.
 </figcaption>
 </figure>
 
+  During this time, they collected hourly measurements of rainfall, landslide movement, landslide thickness changes, groundwater pressure, and swell pressure (the pressure exerted when the soil in the landslide expands, usually due to the presence of water).  They also collected samples of the material (dirt) from the landslide to analyze in a laboratory.  From these samples they learned about the water content, the density, shear strength (how well the material holds together under pressure), type of landslide materials and minerals (most importantly the percentage of clay minerals), the shrink-swell amount, and the swell pressure.
+</p>
+
 <p>
   Previous studies had concluded that pore water pressure, or the amount of water in the open spaces between the dirt particles in the landslide material, controlled the movement of earthflows. If that were the case, one would expect the landslide to start moving when it contained water, and to move the fastest when it contained the most water. However, the data in this study showed that the timing and speed of the earthflow movement did not correlate with the amount of water.   Instead, the initiation of movement was several months after the water amount peaked, and the peak speed occurred when the amount of water was decreasing.  The previously mentioned lab analyses of the samples from the earthflow were essential in illuminating the reasons for this behavior.
 </p>
+
+<figure>
+<img src="images/graph-simplified.png" alt=""/>
+<figcaption>
+  This graph shows that the landslide movement (red) was slow during the times when there was more rainfall (green), the landslide was thicker (orange), and the swell pressure (gray) of the landslide material increased (both due to the clay expanding). The landslide movement increased when those 3 factors were decreasing.
+</figcaption>
+</figure>
+
 <p>
   The earthflow material contained a large amount of expansive clay. Clay swells when it has more water and shrinks when there is less water.  The scientists hypothesized, from the correlation of the swell pressures measured in the laboratory and field with the earthflow movement, that the presence and amount of clay in the earthflow were connected to the timing of the movements and their speed. When there was a large volume of water present, the clay expanded and exerted high pressure on the sides of the landslide, restricting the movement of the earthflow like when brakes are applied to a wheel. Then when the water volume decreased, the clay shrunk, decreasing the swell pressure and allowing the earthflow to start moving again.
 </p>
@@ -76,5 +87,5 @@
 </figure>
 
 <p>
-  Bill is an engineering geologist by training with primary focus on landslides, which he's studied professionally since 1996. He's particularly interested in understanding controls on their speed and timing. &quot;In my free time, I enjoy spending time with my family, running (but I'm very slow), racing my car to make up for my slow running speed (but only on racetracks!), hiking, and being outdoors. In fact, I suppose I started studying landslides as a child when I spent most of my time along the bluffs of Lake Michigan; I always knew when the debris-flow deposits would be fresh, deep and great for playing in!&quot;
+  Bill Schulz is an engineering geologist by training with primary focus on landslides, which he's studied professionally since 1996. He's particularly interested in understanding controls on their speed and timing. &quot;In my free time, I enjoy spending time with my family, running (but I'm very slow), racing my car to make up for my slow running speed (but only on racetracks!), hiking, and being outdoors. In fact, I suppose I started studying landslides as a child when I spent most of my time along the bluffs of Lake Michigan; I always knew when the debris-flow deposits would be fresh, deep and great for playing in!&quot;
 </p>
