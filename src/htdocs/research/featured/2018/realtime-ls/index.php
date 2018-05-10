cd63d1db2de040a4150fda42aea74cd9b687fb08
lisa-usgs <lisa@usgs.gov>
2018-05-10 13:30:54 -0600

    initial commit of empty file

---
+++ src/htdocs/research/featured/2018/realtime-ls/index.php
@@ -0,0 +1,9 @@
+<?php
+// Author: Lisa Wald
+// Contact: Kate Allstadt
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Near Realtime Maps of Possible Earthquake-Triggered Landslides';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
