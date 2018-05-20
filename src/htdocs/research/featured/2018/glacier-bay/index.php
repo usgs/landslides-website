46afd06bd5c6b118f6d04a07e7bc021c75e52598
lisa-usgs <lisa@usgs.gov>
2018-05-20 14:27:38 -0600

    initial commit of webpage with text only

---
+++ src/htdocs/research/featured/2018/glacier-bay/index.php
@@ -0,0 +1,68 @@
+<?php
+// Author: Lisa Wald
+// Contact: Jeff Coe
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Mountain Permafrost, Climate Change, and Rock Avalanches in Glacier Bay National Park, Alaska';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<p>
+  We usually hear about landslides and avalanches that are caused by large amounts of rainfall, the shaking from earthquakes, or a volcanic eruption, but we may be hearing more about avalanches caused by the (seemingly innocuous) melting of ice in the coming years. One such rock avalanche occurred on the Lamplugh glacier in a remote part of Glacier Bay National Park and Preserve in June of 2016. The occurrence was initially known only because it was captured on seismic instruments used to record earthquakes, and it released the same amount of energy as an M5.2 earthquake.
+</p>
+<p>
+  Rock avalanches are landslides of fragmented rock that begin from rock slides and can be extremely hazardous because they are large and can move long distances at extremely rapid speeds.  Southeast Alaska, where Glacier Bay National Park is located, has had a notable number of rock avalanches in recent years, including the largest non-volcanic landslide ever recorded in North America in October of the previous year on the Tyndall Glacier. This rock avalanche generated a tsunami that ran about 600 ft up a shoreline, one of the largest tsunami runups ever recorded.
+</p>
+<p>
+  What is happening in Glacier Bay National Park to suddenly cause more frequent and larger rock avalanches?  Three landslide scientists (from the USGS, CO School of Mines, and Ministry of Forests, Lands, Natural Resource Operations and Rural Development in Canada) examined 33 years of Landsat satellite images to try to find out.  They focused on a peninsular area in the western portion of the Park mostly covered with ice, with the Pacific Ocean on one side and Glacier Bay on the other. They identified rock avalanches on the images by 1) looking for areas with a high contrast to the surrounding ice and snow that would indicate rock avalanche material, 2) large differences between successive images, indicating a change in the topography, and 3) typical shapes indicative of rock avalanches. After identifying 24 landslides, they checked the earthquake record to make sure none of them were caused by earthquakes, since they were only interested in those that had other causes.  None had been caused by earthquakes.
+</p>
+<p>
+  All of the landslides were in the northern part of the study area at high elevations, and 75% of them were on north- to northeast-facing slopes. Those areas are particularly subject to mountain permafrost.  Mountain permafrost is the ice in the cracks and crevices between the rocks that hold them together and help stabilize steep slopes.  There were 3 clusters of activity: 1984-1986, 1994-1995, and 2012-present, and the most recent cluster had more and larger rock avalanches that originated higher on the slopes and travelled further.
+</p>
+<p>
+  After eliminating several other possible causes for the increase in numbers, rock size, and travel distance for the most recent avalanches, the scientists suggest that the melting of the mountain permafrost is the answer. The overall warmer temperatures in the last few decades in this part of Alaska cause the permafrost to degrade or partially melt, destabilizing the slopes and making rock slides and rock avalanches more likely.  The reason why the rock avalanches are initiating at higher elevations and on the north side of slopes is that those are areas that have more mountain permafrost and are most susceptible to changes in long-term temperatures.
+</p>
+<p>
+  These observations and hypotheses can be compared with additional observations in the future to see if they hold up.  If indeed warmer temperatures cause destabilization of steep slopes in traditionally cold climate regions, there will be more high-risk areas for rock slides and rock avalanches in the future.
+</p>
+<p>- written by Lisa Wald, U.S. Geological Survey</p>
+
+<h2>For More Information</h2>
+<ul>
+  <li>
+    <a href="https://link.springer.com/article/10.1007/s10346-017-0879-7">Increasing rock-avalanche size and mobility in Glacier Bay National Park and Preserve, Alaska detected from 1984 to 2016 Landsat imagery</a>
+  </li>
+  <li>
+    <a href="https://www.adn.com/alaska-news/science/2017/09/18/as-ice-thaws-rock-avalanches-on-southeast-alaska-mountains-are-getting-bigger/">As ice thaws, rock avalanches on Southeast Alaska mountains are getting bigger</a>
+  </li>
+  <li>
+    http://www.ldeo.columbia.edu/news-events/massive-landslide-detected-glacier-bay%E2%80%99s-fragile-mountains
+  </li>
+  <li>
+    https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1002/2017JF004512
+  </li>
+  <li>
+    <a href="https://blogs.agu.org/landslideblog/2017/09/12/rock-avalanche-alaska/">Increasing rock avalanche size and mobility in Alaska may be associated with climate change</a>
+  </li>
+  <li>
+    <a href="https://blogs.agu.org/landslideblog/2016/01/02/tyndall-glacier-landslide-1/">The Tyndall Glacier landslide in Alaska - AGU Blogosphere</a>
+  </li>
+  <li>
+    <a href="https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1002/2017JF004512">Using stereo satellite imagery to account for ablation, entrainment, and compaction in volume calculations for rock avalanches on glaciers: Application to the 2016 Lamplugh rock avalanche in Glacier Bay National Park, Alaska</a>
+  </li>
+  <li>
+    <a href="https://www.hakaimagazine.com/features/when-mountains-fall-into-the-sea/">When mountains fall into the sea</a>
+  </li>
+  <li>
+    <a href="http://dggs.alaska.gov/webpubs/dggs/ic/text/ic065.pdf">Landslides in Alaska</a>
+  </li>
+</ul>
+
+<h2>The USGS Scientists Behind the Science</h2>
+<p>
+  Jeff Coe is a geologist who has been with the USGS for 30 years. Twenty-two of these years have been spent researching landslide processes and hazards. In his free time, Jeff enjoys building rock walls, hiking, and pretty much everything else outdoors.
+</p>
+<p>
+  Erin Bessette-Kirton has worked at the USGS for two years, researching landslides in many parts of the US and around the world. She enjoys hiking, mountain biking, running, skiing, and exploring Colorado.
+</p>
