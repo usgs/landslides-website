d0f64f7467f9e5a7692d8b312bde70d362833f67
lisa-usgs <lisa@usgs.gov>
2018-06-05 15:47:59 -0600

    edits from Jill

--- src/htdocs/research/featured/2018/clayey-ls/index.php
+++ src/htdocs/research/featured/2018/clayey-ls/index.php
@@ -9,7 +9,7 @@
 ?>
 
 <p>
-  <a href="clay.pdf">
+  <a href="slowmotion-ls.pdf">
     <i class="material-icons">save_alt</i> Download PDF
   </a>
 </p>
@@ -22,7 +22,7 @@
 </figure>
 
 <p>
-  Not all landslides occur as catastrophic events. Some creep along at a snail’s pace, stopping and starting, and not advancing more than 1 meter per year, and many of these are called earthflows.  Earthflows are extremely common all over the world, in fact they’re much more common than the fast-moving, deadly landslides we hear about often.  Since earthflows rarely cause loss of life, they are rarely in the news like their deadly counterparts, but they do cause a great deal of damage worldwide. Despite the fact that earthflows are so common, scientists still haven’t completely nailed down the reasons why they are so sluggish and sometimes move in fits and starts. Improved understanding of these reasons not only helps reduce hazards from earthflow movement, but also aids understanding of controls on many types of landslides. USGS scientist Bill Schulz and engineer Joel Smith, and several colleagues from other institutions, studied one of thousands of earthflows in Northern California by the name of Two Towers to see if they could learn more about what controls their behavior.
+  Not all landslides occur as catastrophic events. Some creep along at a snail’s pace, stopping and starting, and not advancing more than 1 meter per year, and many of these are called earthflows.  Earthflows are extremely common all over the world, in fact they’re much more common than the fast-moving, deadly landslides.  Since earthflows rarely cause loss of life, they are rarely in the news like their deadly counterparts, but they do cause a great deal of damage worldwide. Despite the fact that earthflows are so common, scientists still haven’t completely nailed down the reasons why they are so sluggish and sometimes move in fits and starts. USGS scientist Bill Schulz and engineer Joel Smith, and several colleagues from other institutions, studied one of thousands of earthflows in Northern California by the name of Two Towers to see if they could learn more about what controls its behavior.
 </p>
 <p>
   The Two Towers earthflow is about 250m long, about 40m wide, and 7m deep and moves under 1m per year. It’s typical of many of the other earthflows in the area near Zenia, CA, and it was chosen for this study because it moves slowly, has a simple shape, and is small enough to accommodate the small number of instruments the researchers had available to dedicate to a multi-year study. Such a slow-moving landslide requires a long time and a lot of patience to collect adequate data for a study, and the team collected data for roughly 2-1/2 years from 2014 to 2017.
@@ -39,7 +39,7 @@
 </p>
 
 <p>
-  Previous studies had concluded that pore water pressure, or the pressure of water in the open spaces between the dirt particles in the landslide material, controlled the movement of all landslides. If that were the case, it follows that one would expect the landslide to start moving when it contained enough water, and to move the fastest when it contained the most water. However, the data in this study showed that the timing and speed of the earthflow movement did not correlate with the amount of water.   Instead, the initiation of movement was several months after the water amount peaked, and the peak speed occurred when the amount of water was decreasing.  The previously mentioned lab analyses of the samples from the earthflow were essential in illuminating the reasons for this behavior.
+  Previous studies concluded that pore water pressure, or the pressure of water in the open spaces between the dirt particles in the landslide material, controlled landslide movement. If that were the case, it follows that one would expect the landslide to start moving when it contained enough water, and to move the fastest when it contained the most water. However, the data in this study showed that the timing and speed of the earthflow movement did not correlate with the amount of water.   Instead, the initiation of movement was several months after the water amount peaked, and the peak speed occurred when the amount of water was decreasing.  The previously mentioned lab analyses of the samples from the earthflow were essential in illuminating the reasons for this behavior.
 </p>
 
 <figure>
@@ -48,10 +48,6 @@
   This graph shows that the landslide movement (red) was slow during the times when there was more rainfall (green), the landslide was thicker (orange), and the swell pressure (gray) of the landslide material increased (both due to the clay expanding). The landslide movement increased when those 3 factors were decreasing.
 </figcaption>
 </figure>
-
-<p>
-  The earthflow material contained a large amount of expansive clay. Clay swells when it has more water and shrinks when there is less water.  The scientists hypothesized, from the correlation of the swell pressures measured in the laboratory and field with the earthflow movement, that the presence and amount of clay in the earthflow were connected to the timing of the movements and their speed. When there was a large volume of water present, the clay expanded and exerted high pressure on the sides of the landslide, restricting the movement of the earthflow like when brakes are applied to a wheel. Then when the water volume decreased, the clay shrunk, decreasing the swell pressure and allowing the earthflow to accelerate.
-</p>
 
 <figure class='right'>
 <img src="images/hole.jpg" alt=""/>
@@ -67,12 +63,12 @@
 </figcaption>
 </figure>
 
+<p>
+  The earthflow material contained a large amount of expansive clay. Clay swells when it has more water and shrinks when there is less water.  The scientists hypothesized, from the correlation of the swell pressures measured in the laboratory and field with the earthflow movement, that the presence and amount of clay in the earthflow were connected to the timing and speed of the movements. When there was a large volume of water present, the clay expanded and exerted high pressure on the sides of the landslide, restricting the movement of the earthflow like when brakes are applied to a wheel. Then when the water volume decreased, the clay shrunk, decreasing the swell pressure and allowing the earthflow to accelerate.
+</p>
 
 <p>
-  Although clays and soil swelling may play a role in the timing and speed of many landslides, there is still a great deal to learn about this phenomenon and the exact details of how it works. Presumably the amount and type of clay matters, and the shape of the earthflow also matters. The interplay of the amount of water (pore pressure) and amount and type of clay (swell pressure) are complex, and the effect it has on the other properties of the material, such as its strength to hold together, may be complicated.  There are so many other factors that influence landslide behavior, and it’s difficult to determine the relative influence each has when each landslide is unique.
-</p>
-<p>
-  All steep slopes may look the same, but they don’t behave the same. Some steep slopes have the potential to fail suddenly and quickly and cause loss of life, and others are more lethargic and pose a lower safety hazard. Many factors that cause earthflow acceleration also affect other types of landslides, including those that move rapidly, so the findings from this study may have wide applicability. The results will also help identify which slopes pose the highest hazards.
+  All steep slopes may look the same, but they don’t behave the same. Some steep slopes have the potential to fail suddenly and quickly and cause loss of life, and other steep slopes are more lethargic and pose a lower safety hazard. Many factors that cause earthflow acceleration also affect other types of landslides, including those that move rapidly, so the findings from this study may have wide applicability. The results will also help identify which slopes pose the highest hazards.
 </p>
 
 <p>-written by Lisa Wald</p>
