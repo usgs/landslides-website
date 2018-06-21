d81b68c3ec7f6905bfbfdfd45ff8fdb342d8f0d6
lisa-usgs <lisa@usgs.gov>
2018-06-21 13:13:17 -0600

    updated name of cooperator

--- src/htdocs/monitoring/bent_creek/index.php
+++ src/htdocs/monitoring/bent_creek/index.php
@@ -99,7 +99,7 @@
       <h2>Cooperators</h2>
 
       <ul>
-        <li>The North Carolina Department of Environment and Natural Resources</li>
+        <li>The North Carolina Department of Environmental Quality</li>
         <li>The United States Forest Service</li>
         <li>Colorado School of Mines (National Science
         Foundation award CMMI 0855783 to N. Lu and J.W. Godt and NASA NNX12AO19G to N.
