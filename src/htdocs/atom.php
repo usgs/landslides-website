46e91e28773588c77a0c529ca7caeb2c2baeeb24
lisa-usgs <lisa@usgs.gov>
2018-10-25 13:00:51 -0600

    removing unneeded files after migration to Drupal CMS

--- src/htdocs/atom.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once '_features.inc.php';
-
-header('Content-Type: application/atom+xml');
-echo $LS_FEATURES->toAtom();
-?>
--- src/htdocs/research/featured/index.php
+++ src/htdocs/research/featured/index.php
@@ -4,10 +4,8 @@
   if (!isset($TEMPLATE)) {
     $TITLE = 'Science for Everyone';
     include_once 'functions.inc.php';
-    ob_start();
-    include '../_navigation.inc.php';
-    $NAVIGATION = ob_get_clean();
     $HEAD = '<link rel="stylesheet" href="/research/featured/styles.css"/>';
+    $NAVIGATION = true;
     include 'template.inc.php';
   }
 ?>
@@ -19,18 +17,25 @@
   echo $LS_FEATURES->toHtml(-1);
   ?>
 
-  <h3>Science for Everyone Archives</h3>
-
-<h4>2017</h4>
-<ul>
-  <li><a href="/research/featured/2017/maria-pr/">Preliminary Locations &amp; Map data showing concentration of landslides caused by Hurricane Maria in Puerto Rico</a>
+<h3>2017</h3>
+<ul class="no-style linklist feature">
+  <li><a href="/research/featured/2017/maria-pr/">
+    <h4>Preliminary Locations &amp; Map data showing concentration of landslides caused by Hurricane Maria in Puerto Rico</h4>
+    <img class="feature-image" src="/research/featured/2017/maria-pr/images/PR_Maria_LS_density_map_sm.jpg" alt=""/>
+  </a>
   </li>
-  <li><a href="/research/featured/2017/nuugaatsiaq/">Preliminary Analysis of Satellite Imagery and Seismic Observations of the Nuugaatsiaq Landslide and Tsunami, Greenland</a>
+  <li><a href="/research/featured/2017/nuugaatsiaq/">
+    <h4>Preliminary Analysis of Satellite Imagery and Seismic Observations of the Nuugaatsiaq Landslide and Tsunami, Greenland</h4>
+    <img class="feature-image" src="/research/featured/2017/nuugaatsiaq/images/nuug-landslide.jpg" alt=""/>
+</a>
   </li>
 </ul>
 
 <h4>2016</h4>
-<ul>
-  <li><a href="/research/featured/2015-westsaltcreek/">Reconstruction of an Avalanche: The West Salt Creek Rock Avalanche, Colorado, May 25, 2014</a>
+<ul class="no-style linklist feature">
+  <li><a href="/research/featured/2015-westsaltcreek/">
+        <h4>Reconstruction of an Avalanche: The West Salt Creek Rock Avalanche, Colorado, May 25, 2014</h4>
+        <img class="feature-image" src="/research/featured/2015-westsaltcreek/images/westsaltcreek-sm.jpg" alt=""/>
+      </a>
   </li>
 </ul>
